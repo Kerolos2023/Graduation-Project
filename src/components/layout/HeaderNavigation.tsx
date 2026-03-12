44a0acc4c36d9cca75b3771d8645c48aa14bad2b
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2026-03-12 13:30:13 +0200

    add studnet link in header navigation

--- src/components/layout/HeaderNavigation.tsx
+++ src/components/layout/HeaderNavigation.tsx
@@ -14,7 +14,7 @@ export const HeaderNavigation = () => {
 
     const navLinks = [
         { label: "College Data", active: isCollegeData, href: "/student-affairs/college-data/courses" },
-        { label: "Students", active: isStudents, href: "#" },
+        { label: "Students", active: isStudents, href: "/student-affairs/students" },
         { label: "Committees & Examinations", active: isCommittees, href: "#" },
     ];
 
