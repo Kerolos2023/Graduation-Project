1243e5652255e46d9bdc1af79ed28197dde72fff
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2026-04-09 13:30:40 +0200

    add student module layout

---
+++ src/app/student/basic-data-profile/page.tsx
@@ -0,0 +1,3 @@
+export default function BasicDataProfilePage() {
+  return <div className="h-full w-full" />;
+}
---
+++ src/app/student/student-progress/page.tsx
@@ -0,0 +1,3 @@
+export default function StudentProgressPage() {
+  return <div className="h-full w-full" />;
+}
